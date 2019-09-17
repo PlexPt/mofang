80e5e2a262e082ce9f3ef53d38dbd09b58aa9b1e
pt <plexpt@gmail.com>
2019-09-17 14:23:53 +0800

    跑起来了

---
+++ src/main/java/com/mf/mofang/config/WebConfig.java
@@ -0,0 +1,32 @@
+package com.mf.mofang.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+/**
+ * 静态资源配置类
+ *
+ * @author pt
+ * @email plexpt@gmail.com
+ * @date 2019-09-17 14:11
+ */
+@Slf4j
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("配置静态资源所在目录");
+        // 和页面有关的静态目录都放在项目的static目录下
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
--- src/main/java/com/mf/mofang/controller/CaseController.java
+++ src/main/java/com/mf/mofang/controller/CaseController.java
@@ -2,12 +2,29 @@ package com.mf.mofang.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author pt
  */
+@Slf4j
 @Controller
 public class CaseController {
 
+
+    /**
+     * 跳转case案例页面
+     */
+    @RequestMapping(value = "/case")
+    public ModelAndView toDemo(ModelAndView mv) {
+        log.info("====>>跳转case页面");
+        mv.addObject("title", "案例");
+        mv.addObject("content", "  <h1>抱歉，您访问的案例不存在或已被删除</h1>");
+        mv.setViewName("case");
+        return mv;
+    }
 
 }
