3e2515c6a70fa90b1d3dd4dc35fb2432bc589c50
pt <plexpt@gmail.com>
2019-09-17 13:34:02 +0800

    配置

--- src/main/java/com/mf/mofang/controller/IndexControoler.java
+++ src/main/java/com/mf/mofang/controller/IndexControoler.java
@@ -3,17 +3,20 @@ package com.mf.mofang.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author pt
  */
 @Controller
+@Slf4j
 @RequestMapping("")
 public class IndexControoler {
 
 
+    @RequestMapping(value = {"/", "/index"})
+    public String index() {
 
-    @RequestMapping("")
-    public String get() {
-        return "";
+        return "index";
     }
 }
