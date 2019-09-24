28acfef4c367cf58f8ee1eaa41a8d45ffe2f78ae
pt <plexpt@gmail.com>
2019-09-24 15:48:02 +0800

    add配置文件

--- src/main/java/com/mf/mofang/MofangApplication.java
+++ src/main/java/com/mf/mofang/MofangApplication.java
@@ -3,7 +3,8 @@ package com.mf.mofang;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import java.io.File;
 
 // 下面这一行为新增数据库关联注解
 @SpringBootApplication
@@ -11,6 +12,22 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class MofangApplication {
 
     public static void main(String[] args) {
+
+        // 文件上传的路径
+        String filePath = "==";
+
+        String currentpath = System.getProperty("user.dir");
+        String absolutePath = new File("").getAbsolutePath();
+
+        if (!currentpath.contains("bin")) {
+            System.out.println("路径配置不正确,将使用默认");
+        } else {
+            String b = currentpath.substring(0, currentpath.indexOf("bin")) + "www\\file\\";
+            System.out.println("文件上传的路径 " + b);
+            filePath = b;
+        }
+
+
         SpringApplication.run(MofangApplication.class, args);
     }
 
